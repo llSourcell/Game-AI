57e26c58dbd87bff7a1f787bfde4ce1dd27f2432
Siraj <sirajravel@gmail.com>
2016-05-17 15:23:39 -0700

    Update train.py

--- train.py
+++ train.py
@@ -18,22 +18,24 @@ class Trainer:
             failures = 0
             total_loss = 0
 
-            print "start %d random plays to populate replay memory" % self.agent.replay_start_size
+            print "starting %d random plays to populate replay memory" % self.agent.replay_start_size
             for i in xrange(self.agent.replay_start_size):
                 # follow random policy
                 state, action, reward, next_state, terminal = self.agent.observe(1)
 
                 if reward == 1:
                     successes += 1
-                elif reward == -1:
+                elif terminal:
                     failures += 1
 
                 if (i+1) % 10000 == 0:
-                    print "memory size: %d" % len(self.agent.memory)
+                    print "\nmemory size: %d" % len(self.agent.memory),\
+                          "\nSuccesses: ", successes,\
+                          "\nFailures: ", failures
             
             sample_success = 0
             sample_failure = 0
-            print "start training..."
+            print "\nstart training..."
             start_time = time.time()
             for i in xrange(self.agent.train_steps):
                 # annealing learning rate
@@ -49,7 +51,7 @@ class Trainer:
 
                 if reward == 1:
                     successes += 1
-                elif reward == -1:
+                elif terminal:
                     failures += 1
                 
                 if ((i+1) % self.agent.save_weights == 0):
@@ -58,14 +60,14 @@ class Trainer:
                 if ((i+1) % self.agent.batch_size == 0):
                     avg_loss = total_loss / self.agent.batch_size
                     end_time = time.time()
-                    print "memory size: ", len(self.agent.memory),\
-                          "\nTraining steps: ", i+1,\
+                    print "\nTraining step: ", i+1,\
+                          "\nmemory size: ", len(self.agent.memory),\
                           "\nLearning rate: ", lr,\
                           "\nSuccesses: ", successes,\
                           "\nFailures: ", failures,\
-                          "\nSuccesse samples: ", sample_success,\
-                          "\nFailure samples: ", sample_failure,\
+                          "\nSample successes: ", sample_success,\
+                          "\nSample failures: ", sample_failure,\
                           "\nAverage batch loss: ", avg_loss,\
-                          "\nBatch training time: ", (end_time-start_time)/self.agent.batch_size
+                          "\nBatch training time: ", (end_time-start_time)/self.agent.batch_size, "s"
                     start_time = time.time()
                     total_loss = 0
